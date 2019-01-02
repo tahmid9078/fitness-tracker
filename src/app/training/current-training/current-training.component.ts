0be9a3c2ae26c5357649fa88d0a30ccbeb7b08d4
Tahmid Abdullah <sakib.9078@gmail.com>
2019-01-02 06:31:32 +0600

    added traning components

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { StopTrainingComponent } from './stop-training.component';
 
 @Component({
   selector: 'app-current-training',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurrentTrainingComponent implements OnInit {
 
-  constructor() { }
+  @Output() trainingExit = new EventEmitter();
+  progress = 0;
+  timer: any;
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
+    this.onStartOrResume();
   }
 
+  onStartOrResume() {
+    this.timer = setInterval(()=>{
+      this.progress += 5;
+      if(this.progress >= 100){
+        clearInterval(this.timer);
+      }
+    },1000);
+  }
+
+  onStop(){
+    clearInterval(this.timer);
+    const dialogRef = this.dialog.open(StopTrainingComponent, {
+      data: {
+        progress: this.progress
+      }
+    });
+    dialogRef.afterClosed().subscribe(
+      result =>{
+      if(result){
+        this.trainingExit.emit();
+      }
+      else{
+        this.onStartOrResume();
+      }
+    });
+  }
 }
--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrainingComponent implements OnInit {
 
+  isTrainingRunning: boolean = false;
+  
   constructor() { }
 
   ngOnInit() {
