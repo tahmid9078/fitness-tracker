b07bead2a12029f1c7ab1e06140446860c6b7d19
Tahmid Abdullah <sakib.9078@gmail.com>
2019-01-16 10:10:33 +0600

    refactoring

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -40,7 +40,7 @@ export class TrainingService {
     }
 
     completeExercise() {
-        this.exercises.push(
+        this.addExerciseInDb(
             {...this.runningExercise,
             date: new Date(),
             state: "completed"
@@ -52,7 +52,7 @@ export class TrainingService {
     }
 
     cancelExercise(progress: number) {
-        this.exercises.push({
+        this.addExerciseInDb({
             ...this.runningExercise,
             duration: this.runningExercise.duration * (progress/100),
             calories: this.runningExercise.calories * (progress/100),
@@ -60,7 +60,7 @@ export class TrainingService {
             state: "cancelled"
 
         });
-        console.log(this.exercises[0]);
+        
         this.runningExercise = null;
         this.changeExercise.next(null);
         
@@ -68,5 +68,9 @@ export class TrainingService {
 
     getExercises() {
         return this.exercises.slice();
+    }
+
+    private addExerciseInDb(exercise: Exercise) {
+        this.db.collection("finishedExercises").add(exercise);
     }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,18 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+import { config } from './config';
+
 
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCJd8XSFnwnI6lUr4LwKs77KRIROsJrphw",
-    authDomain: "ng-fitness-tracker-df0ac.firebaseapp.com",
-    databaseURL: "https://ng-fitness-tracker-df0ac.firebaseio.com",
-    projectId: "ng-fitness-tracker-df0ac",
-    storageBucket: "ng-fitness-tracker-df0ac.appspot.com",
-    messagingSenderId: "102302978659"
+    apiKey: config.apiKey,
+    authDomain: config.authDomain,
+    databaseURL: config.databaseURL,
+    projectId: config.projectId,
+    storageBucket: config.storageBucket,
+    messagingSenderId: config.messagingSenderId
 
   }
 };
